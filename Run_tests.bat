ab1f4a0107545f214f7c01d12d32572017d24364
Melnichenko Oksana <melnicenkooksana1990@gmail.com>
2024-01-15 18:29:44 +0100

    error correction

--- Run_tests.bat
+++ Run_tests.bat
@@ -1,22 +1,22 @@
 @echo off
 
 REM Запуск Appium Server
-echo Запуск Appium Server...
+echo Starting the Appium Server...
 start "Appium Server" cmd /k appium
 
 REM Задержка, чтобы Appium Server запустился
 timeout /t 10
 
 REM Запуск Android Emulator
-echo Запуск Android Emulator...
+echo Starting the Android Emulator...
 start "Android Emulator" cmd /k emulator -avd Medium_Phone_API_33
 
 REM Задержка, чтобы эмулятор полностью загрузился
-timeout /t 20
+timeout /t 40
 
 REM Запуск тестов
-echo Запуск тестов...
-pytest calculator_tests2.py
+echo Starting tests...
+pytest calculator_tests.py
 
 timeout /t 20
 
